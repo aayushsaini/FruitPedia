ab0efa99abc93f66b098d163fa3bd3142817bb1c
aayushsaini <aayush.r98@gmail.com>
2021-02-05 23:57:13 +0530

    Initial Commit

---
+++ FruitPedia/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FruitPedia
+//
+//  Created by Aayush Saini on 05/02/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FruitPedia/FruitPediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitPediaApp.swift
+//  FruitPedia
+//
+//  Created by Aayush Saini on 05/02/21.
+//
+
+import SwiftUI
+
+@main
+struct FruitPediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
